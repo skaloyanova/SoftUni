82e9116995fcfede703ac88430e8163cf435afc3
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-09-21 21:18:05 +0300

    solution added

---
+++ programming-for-QA-sep2023/02.Loops-Exercise1/01.NumbersEndingIn7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.NumbersEndingIn7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ programming-for-QA-sep2023/02.Loops-Exercise1/03.OldBooks/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.OldBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookToSearch = Console.ReadLine();
+            int bookCounter = 0;
+
+            while (true)
+            {
+                string currentBook = Console.ReadLine();
+                
+
+                if (currentBook == bookToSearch)
+                {
+                    Console.WriteLine($"You checked {bookCounter} books and found it.");
+                    break;
+                }
+
+                if (currentBook == "No More Books")
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {bookCounter} books.");
+                    break;
+                }
+
+                bookCounter++;
+
+            }
+        }
+    }
+}
