fa2abffb08d758ea4dd0bc3ba561e3dda3ff9d32
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-09-21 10:54:44 +0300

    update

---
+++ programming-for-QA-sep2023/02.Loops_Lab/01.NumbersFromOneToN/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.NumbersFromOneToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
