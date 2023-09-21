e5570127a3e0e17b3b9a04b6c95d168712cf38c0
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-09-21 10:15:37 +0300

    solution added

---
+++ programming-for-QA-sep2023/02.Loops_Lab/08.TheGreatestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _08.TheGreatestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                int number = int.Parse(input);
+
+                max = Math.Max(max, number);
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
