3dd8c275fbecc5b6ff9c60fc859fa34758a7336e
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-09-22 18:13:16 +0300

    solution added

---
+++ programming-for-QA-sep2023/02.Loops-Exercise1/09.Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _09.Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int roomVolume = width * length * height;
+
+            while (true) { 
+                string input = Console.ReadLine();
+
+                if (input == "Done")
+                {
+                    Console.WriteLine($"{roomVolume} Cubic meters left.");
+                    break;
+                }
+
+                int boxSize = int.Parse(input);
+
+                roomVolume -= boxSize;
+
+                if (roomVolume < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(roomVolume)} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
