f9a296ef1838b22c0d05154e2ae543b3bf18e661
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-09-23 15:13:05 +0300

    solution added

--- programming-for-QA-sep2023/02.Loops_Exercise2/01.NumberPyramid/Program.cs
+++ programming-for-QA-sep2023/02.Loops_Exercise2/01.NumberPyramid/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            int printNum = 1;
+            bool exitLoop = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {                 
+                    if (printNum > n)
+                    {
+                        exitLoop = true;
+                        break;
+                    }
+                    Console.Write(printNum + " ");
+                    printNum++;
+                }
+                if (exitLoop)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ programming-for-QA-sep2023/02.Loops_Exercise2/05.SpecialNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //generates all possible "special" numbers from 1111 to 9999. For a number to be "special", N should be divisible by each of its digits without a remainder
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int num = i;
+                bool isSpecial = true;
+
+                while (num > 0)
+                {
+                    int lastDigit = num % 10;
+                    num = num / 10;
+
+                    if (lastDigit == 0 || n % lastDigit != 0)   //If a number contains zero as any of its digits, it’s never a special number
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
