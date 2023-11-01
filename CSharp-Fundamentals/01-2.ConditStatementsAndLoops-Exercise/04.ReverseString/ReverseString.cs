298590a3d559a7f4d95de5c3ed7039eef33888aa
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-01 19:58:44 +0200

    solutions added

---
+++ CSharp-Fundamentals/01-2.ConditStatementsAndLoops-Exercise/04.ReverseString/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+string text = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+for (int i = text.Length - 1; i >= 0; i--)
+{
+    sb.Append(text[i]);
+}
+
+Console.WriteLine(sb);
