b07afbeb8268756cbeb23a233242d096e4e084cb
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-03 14:33:30 +0200

    update

--- Programming-for-QA-sep2023/11.ExamPreparation/11.SumFactorialEvenDigits/SumFactorialEvenDigits.cs
+++ Programming-for-QA-sep2023/11.ExamPreparation/11.SumFactorialEvenDigits/SumFactorialEvenDigits.cs
@@ -5,6 +5,9 @@
  * Ex: 4532 -> 4! + 2! = 26
  */
 
+
+// VAR 1
+/*
 int num = int.Parse(Console.ReadLine());
 
 long sum = 0;
@@ -21,14 +24,26 @@ while (num > 0)
 }
 
 Console.WriteLine(sum);
+*/
+
+// VAR 2
+long result = Console.ReadLine()
+	.ToCharArray()
+	.Select(c => c - '0')
+	.Where(c => c % 2 == 0)
+	.Select(c => Factorial(c))
+	.Sum();
+
+Console.WriteLine(result);
 
 
+// Method for calculating Factorial
 static long Factorial(int n)
 {
-	if (n == 1)
-	{
-		return 1;
-	}
-	
-	return n * Factorial(n - 1);
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return n * Factorial(n - 1);
 }
