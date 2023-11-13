c2ac319a957e0d585678151373d3f07aac746c2a
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-13 13:39:11 +0200

    solutions added

---
+++ Programming-for-QA-sep2023/13.StringsTextProcessingRegEx/06.MatchFullName/MatchFullName.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Reads a text from the console
+ * Match full names from the given text
+ * A valid full name has the following characteristics:
+ *     It consists of two words
+ *     Each word starts with a capital letter
+ *     After the first letter, it only contains lowercase letters afterward
+ *     Each of the two words should be at least two letters long
+ *     The two words are separated by a single space
+ * Print full names on the console, separated by single space
+ */
+
+using System.Text.RegularExpressions;
+
+
+string text = Console.ReadLine();
+
+string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+
+foreach (Match m in Regex.Matches(text, pattern))
+{
+    Console.Write($"{m} ");
+}
---
+++ Programming-for-QA-sep2023/13.StringsTextProcessingRegEx/07.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Reads a text from the console
+ * Create a regular expression to match a valid phone number from Sofia
+ * A valid number has the following characteristics:
+ *    It starts with "+359"
+ *    Then, it is followed by the area code (always 2)
+ *    After that, it's followed by the number itself:
+ *    The number consists of 7 digits (separated into two groups of 3 and 4 digits respectively) 
+ *    The different parts are separated by either a space or a hyphen ('-')
+ * Print all valid numbers on the console, separated by a comma and a space ", "
+ */
+
+using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"\+359(?<sep>[\s|-])(2)\<sep>([0-9]{3})\<sep>([0-9]{4})\b";
+
+ICollection<Match> matches = Regex.Matches(text, pattern);
+
+Console.WriteLine(string.Join(", ", matches));
+
