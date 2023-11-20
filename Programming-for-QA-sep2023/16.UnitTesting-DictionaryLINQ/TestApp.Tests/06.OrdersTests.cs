5ded78e6c6f07cf946af8ab273ea46792f50bf1d
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-20 19:24:44 +0200

    update

--- Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/06.OrdersTests.cs
+++ Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/06.OrdersTests.cs
@@ -20,7 +20,18 @@ public class OrdersTests
     [Test]
     public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
     {
-        string[] input = new string[] { "table 1.5 10", "stool 1.55 5", "window 5.1 5", "window 1 0", "door 2.4 2", "door 2.4 4", "zero 0 5", "test 0 4", "test 1.12 5" };
+        string[] input = new string[]
+        {
+            "table 1.5 10",
+            "stool 1.55 5",
+            "window 5.1 5",
+            "window 1 0",
+            "door 2.4 2",
+            "door 2.4 4",
+            "zero 0 5",
+            "test 0 4",
+            "test 1.12 5"
+        };
         StringBuilder sb = new();
         sb.AppendLine("table -> 15.00");
         sb.AppendLine("stool -> 7.75");
@@ -39,7 +50,13 @@ public class OrdersTests
     [Test]
     public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
     {
-        string[] input = new string[] { "table 1.5049 9.3", "stool 1.998 5", "door 2.4 2", "door 2.40148 4" };
+        string[] input = new string[]
+        {
+            "table 1.5049 9.3",
+            "stool 1.998 5",
+            "door 2.4 2",
+            "door 2.40148 4"
+        };
         StringBuilder sb = new();
         sb.AppendLine("table -> 14.00");
         sb.AppendLine("stool -> 9.99");
@@ -55,7 +72,13 @@ public class OrdersTests
     [Test]
     public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
     {
-        string[] input = new string[] { "table 1.5 10.0", "stool 1.55 5.1", "window 5.1 5", "window 5.11 2.1" };
+        string[] input = new string[]
+        {
+            "table 1.5 10.0",
+            "stool 1.55 5.1",
+            "window 5.1 5",
+            "window 5.11 2.1"
+        };
         StringBuilder sb = new();
         sb.AppendLine("table -> 15.00");
         sb.AppendLine("stool -> 7.91");
