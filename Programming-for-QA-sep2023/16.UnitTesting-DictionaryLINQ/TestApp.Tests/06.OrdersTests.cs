fe07a2d455b7e1cc1f9dee13b5fd19955aeb33a9
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-20 19:13:42 +0200

    unit tests added

--- Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/06.OrdersTests.cs
+++ Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/06.OrdersTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using NUnit.Framework;
 
 namespace TestApp.Tests;
@@ -8,31 +9,62 @@ public class OrdersTests
     [Test]
     public void Test_Order_WithEmptyInput_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        string[] input = Array.Empty<string>();
+
+        string result = Orders.Order(input);
+
+        Assert.That(result, Is.Empty);
     }
 
-    // TODO: finish test
+
     [Test]
     public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
     {
-        // Arrange
+        string[] input = new string[] { "table 1.5 10", "stool 1.55 5", "window 5.1 5", "window 1 0", "door 2.4 2", "door 2.4 4", "zero 0 5", "test 0 4", "test 1.12 5" };
+        StringBuilder sb = new();
+        sb.AppendLine("table -> 15.00");
+        sb.AppendLine("stool -> 7.75");
+        sb.AppendLine("window -> 5.00");
+        sb.AppendLine("door -> 14.40");
+        sb.AppendLine("zero -> 0.00");
+        sb.AppendLine("test -> 10.08");
 
-        // Act
-        //string result = Orders.Order(input);
+        string expected = sb.ToString().Trim();
 
-        // Assert
-        //Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+        string result = Orders.Order(input);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
     {
-        // TODO: finish test
+        string[] input = new string[] { "table 1.5049 9.3", "stool 1.998 5", "door 2.4 2", "door 2.40148 4" };
+        StringBuilder sb = new();
+        sb.AppendLine("table -> 14.00");
+        sb.AppendLine("stool -> 9.99");
+        sb.AppendLine("door -> 14.41");
+
+        string expected = sb.ToString().Trim();
+
+        string result = Orders.Order(input);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
     {
-        // TODO: finish test
+        string[] input = new string[] { "table 1.5 10.0", "stool 1.55 5.1", "window 5.1 5", "window 5.11 2.1" };
+        StringBuilder sb = new();
+        sb.AppendLine("table -> 15.00");
+        sb.AppendLine("stool -> 7.91");
+        sb.AppendLine("window -> 36.28");
+
+        string expected = sb.ToString().Trim();
+
+        string result = Orders.Order(input);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
--- Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/07.PlantsTests.cs
+++ Programming-for-QA-sep2023/16.UnitTesting-DictionaryLINQ/TestApp.Tests/07.PlantsTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 
 using System;
+using System.Text;
 
 namespace TestApp.Tests;
 
@@ -9,30 +10,85 @@ public class PlantsTests
     [Test]
     public void Test_GetFastestGrowing_WithEmptyArray_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        string[] input = { };
+
+        string result = Plants.GetFastestGrowing(input);
+
+        Assert.That(result, Is.Empty);
     }
 
-    // TODO: finish test
     [Test]
     public void Test_GetFastestGrowing_WithSinglePlant_ShouldReturnPlant()
     {
-        // Arrange
+        string[] input = { "plant" };
+        string expected = $"Plants with 5 letters:{Environment.NewLine}plant";
 
-        // Act
-        //string result = Plants.GetFastestGrowing(plants);
+        string result = Plants.GetFastestGrowing(input);
 
-        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMultiplePlants_ShouldReturnGroupedPlants()
     {
-        // TODO: finish test
+        string[] input = { "plant", "plant6", "alabaa", "abcde", "four" };
+        StringBuilder sb = new ();
+        sb.AppendLine("Plants with 4 letters:");
+        sb.AppendLine("four");
+        sb.AppendLine("Plants with 5 letters:");
+        sb.AppendLine("plant");
+        sb.AppendLine("abcde");
+        sb.AppendLine("Plants with 6 letters:");
+        sb.AppendLine("plant6");
+        sb.AppendLine("alabaa");
+
+        string expected = sb.ToString().Trim();
+
+        string result = Plants.GetFastestGrowing(input);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMixedCasePlants_ShouldBeCaseInsensitive()
     {
-        // TODO: finish test
+        string[] input = { "plant", "plAnt6", "alaAAb", "Abcde", "f0ur" };
+        StringBuilder sb = new();
+        sb.AppendLine("Plants with 4 letters:");
+        sb.AppendLine("f0ur");
+        sb.AppendLine("Plants with 5 letters:");
+        sb.AppendLine("plant");
+        sb.AppendLine("Abcde");
+        sb.AppendLine("Plants with 6 letters:");
+        sb.AppendLine("plAnt6");
+        sb.AppendLine("alaAAb");
+
+        string expected = sb.ToString().Trim();
+
+        string result = Plants.GetFastestGrowing(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithPlantsWithSpace_ShouldReturnGroupedPlants()
+    {
+        string[] input = { "pla nt", "", "plAnt6", "  ", "1" };
+        StringBuilder sb = new();
+        sb.AppendLine("Plants with 0 letters:");
+        sb.AppendLine("");
+        sb.AppendLine("Plants with 1 letters:");
+        sb.AppendLine("1");
+        sb.AppendLine("Plants with 2 letters:");
+        sb.AppendLine("  ");
+        sb.AppendLine("Plants with 6 letters:");
+        sb.AppendLine("pla nt");
+        sb.AppendLine("plAnt6");
+
+        string expected = sb.ToString().Trim();
+
+        string result = Plants.GetFastestGrowing(input);
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
