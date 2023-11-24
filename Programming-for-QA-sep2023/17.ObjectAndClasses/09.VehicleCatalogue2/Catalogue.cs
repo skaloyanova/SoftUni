7e945f9c40ccb4731f7f4fe72caecad1e1d1b539
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-24 19:18:00 +0200

    solution added

---
+++ Programming-for-QA-sep2023/17.ObjectAndClasses/09.VehicleCatalogue2/Catalogue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.VehicleCatalogue2
+{
+    public class Catalogue
+    {
+        public List<Vehicle> Cars { get; set; }
+        public List<Vehicle> Trucks { get; set; }
+
+        public Catalogue()
+        {
+            this.Cars = new();
+            this.Trucks = new();
+        }
+
+        public double GetCarsAverageHorsePower ()
+        {
+            if (Cars.Count == 0)
+            {
+                return -1;
+            }
+
+            return Cars.Average(c => c.HorsePower);
+        }
+
+        public double GetTrucksAverageHorsePower()
+        {
+            if (Trucks.Count == 0)
+            {
+                return -1;
+            }
+
+            return Trucks.Average(c => c.HorsePower);
+        }
+
+        public Vehicle GetVehicle(string model)
+        {
+            var vehicle = Cars.Find(c => c.Model == model);
+            if (vehicle != null)
+            {
+                return vehicle;
+            }
+
+            return Trucks.Find(t => t.Model == model);
+        }
+    }
+}
---
+++ Programming-for-QA-sep2023/17.ObjectAndClasses/09.VehicleCatalogue2/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.VehicleCatalogue2
+{
+    public class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double HorsePower { get; set; }
+
+        public Vehicle(string type, string model, string color, double horsePower)
+        {
+            this.Type = char.ToUpper(type[0]) + type.Substring(1);    //changing "car/truck" to "Car/Truck"
+            this.Model = model;
+            this.Color = color;
+            this.HorsePower = horsePower;
+        }
+
+        public string GetVehicleDetails()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Type: {this.Type}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Color: {this.Color}");
+            sb.AppendLine($"Horsepower: {this.HorsePower}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
