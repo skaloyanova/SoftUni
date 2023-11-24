ca9996d4f52d99238dcfa85cecf7f67999b5dc10
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-11-24 09:18:56 +0200

    solution added

---
+++ Programming-for-QA-sep2023/17.ObjectAndClasses/05.CarManufacturer/Car.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        //Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+
+        //Constructors
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+
+        // Methods
+        public void Drive(double distance)
+        {
+            double fuelNeeded = distance * this.FuelConsumption;
+
+            if (this.FuelQuantity > fuelNeeded)
+            {
+                this.FuelQuantity -= fuelNeeded;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Programming-for-QA-sep2023/17.ObjectAndClasses/05.CarManufacturer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            /* TASK 1 */
+
+            //Car car = new Car();
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //Console.WriteLine($"Make: {car.Make}");
+            //Console.WriteLine($"Model: {car.Model}");
+            //Console.WriteLine($"Year: {car.Year}");
+
+            /* TASK 2 */
+
+            //Car car = new Car();
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //car.FuelQuantity = 200;
+            //car.FuelConsumption = 200;
+
+            //car.Drive(2000);
+            //Console.WriteLine(car.WhoAmI());
+
+            /* TASK 3 */
+
+            //string make = Console.ReadLine();
+            //string model = Console.ReadLine();
+            //int year = int.Parse(Console.ReadLine());
+            //double fuelQuantity = double.Parse(Console.ReadLine());
+            //double fuelConsumption = double.Parse(Console.ReadLine());
+
+            //Car firstCar = new Car();
+            //Car secondCar = new Car(make, model, year);
+            //Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+            /* TASK 4 */
+
+            Tire[] tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3),
+            };
+
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+        }
+    }
+}
