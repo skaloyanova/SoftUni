a5ccdd4896f87aa12e38534ba77c4ffb9d9978fa
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-12-09 21:11:05 +0200

    solution added

---
+++ Programming-for-QA-sep2023/20.Encapsulation-Inheritance-Abstraction-Polymorphism/01.PersonInfo/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PersonInfo;
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+    private int _age;
+
+    private const int NAME_MIN_LENGTH = 3;
+
+    public string FirstName
+    {
+        get { return this._firstName; }
+        private set
+        {
+            if (value.Length < NAME_MIN_LENGTH)
+            {
+                throw new ArgumentException($"First name cannot contain fewer than {NAME_MIN_LENGTH} symbols!");
+            }
+            this._firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get { return this._lastName; }
+        private set
+        {
+            if (value.Length < NAME_MIN_LENGTH)
+            {
+                throw new ArgumentException($"Last name cannot contain fewer than {NAME_MIN_LENGTH} symbols!");
+            }
+            this._lastName = value;
+        }
+    }
+    public int Age
+    {
+        get { return this._age; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+            _age = value;
+        }
+    }
+
+
+    public Person(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+}
---
+++ Programming-for-QA-sep2023/20.Encapsulation-Inheritance-Abstraction-Polymorphism/02.BoxData/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace BoxData;
+
+public class Box
+{
+    private double _length;
+    private double _width;
+    private double _height;
+
+    public double Length
+    {
+        get { return this._length; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Length cannot be zero or negative.");
+            }
+            this._length = value;
+        }
+    }
+    public double Width
+    {
+        get { return this._width; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Width cannot be zero or negative.");
+            }
+            this._width = value;
+        }
+    }
+    public double Height
+    {
+        get { return this._height; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"Height cannot be zero or negative.");
+            }
+            this._height = value;
+        }
+    }
+
+    public Box(double length, double width, double height)
+    {
+        this.Length = length;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double SurfaceArea()
+    {
+        return (2 * Length * Width) + (2 * Length * Height) + (2 * Width * Height);
+    }
+
+    public double Volume()
+    {
+        return Length * Width * Height;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Surface Area – {this.SurfaceArea():f2}");
+        sb.AppendLine($"Volume – {this.Volume():f2}");
+
+        return sb.ToString().Trim();
+    }
+}
