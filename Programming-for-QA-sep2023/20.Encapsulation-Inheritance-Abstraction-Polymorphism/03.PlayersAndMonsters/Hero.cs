e1f624dbab172f0078cb87d16818f8592dc7c43b
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-12-10 19:19:28 +0200

    solution added

---
+++ Programming-for-QA-sep2023/20.Encapsulation-Inheritance-Abstraction-Polymorphism/03.PlayersAndMonsters/Hero.cs
@@ -0,0 +1,43 @@
+﻿namespace Players;
+
+public class Hero
+{
+    public string UserName { get; set; }
+    public int Level { get; set; }
+
+    public Hero(string userName, int level)
+    {
+        this.UserName = userName;
+        this.Level = level;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: {this.GetType().Name} Username: {this.UserName} Level: {this.Level}";
+    }
+}
+
+public class Elf : Hero
+{
+    public Elf(string userName, int level) : base(userName, level) { }
+}
+
+public class MuseElf : Elf
+{
+    public MuseElf(string userName, int level) : base(userName, level) { }
+}
+
+public class Knight : Hero
+{
+    public Knight(string userName, int level) : base(userName, level) { }
+}
+
+public class DarkKnight : Knight
+{
+    public DarkKnight(string userName, int level) : base(userName, level) { }
+}
+
+public class BladeKnight : DarkKnight
+{
+    public BladeKnight(string userName, int level) : base(userName, level) { }
+}
