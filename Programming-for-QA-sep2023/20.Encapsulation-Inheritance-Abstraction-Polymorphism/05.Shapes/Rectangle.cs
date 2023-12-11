109df68f77d9c50dc3e3eb7fbc36011c4d4dcd50
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2023-12-11 14:36:39 +0200

    solution added

---
+++ Programming-for-QA-sep2023/20.Encapsulation-Inheritance-Abstraction-Polymorphism/05.Shapes/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using Shapes;
+using System;
+using System.Text;
+
+public class Rectangle : IDrawable
+{
+    private int _width;
+    private int _height;
+
+    public Rectangle(int width, int height)
+    {
+        if (width <= 0 || height <= 0)
+        {
+            throw new ArgumentException();
+        }
+        this._width = width;
+        this._height = height;
+    }
+    public void Draw()
+    {
+        StringBuilder sb = new StringBuilder();
+        string firstLastLine = new string('*', _width);
+        string midLine = "*";
+        if (_width > 1)
+        {
+            midLine += new string(' ', _width - 2) + '*';
+        }
+        Console.WriteLine(firstLastLine);
+
+        for (int i = 1; i <= _height - 2; i++)
+        {
+            Console.WriteLine(midLine);
+        }
+
+        Console.WriteLine(firstLastLine);
+    }
+}
---
+++ Programming-for-QA-sep2023/20.Encapsulation-Inheritance-Abstraction-Polymorphism/06.Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cars;
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Battery { get; set; }
+
+    public Tesla(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public Tesla(string model, string color, int battery) : this(model, color)
+    {
+        this.Battery = battery;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Break!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries{Environment.NewLine}" +
+            $"{this.Start()}{Environment.NewLine}" +
+            $"{this.Stop()}";
+    }
+}
