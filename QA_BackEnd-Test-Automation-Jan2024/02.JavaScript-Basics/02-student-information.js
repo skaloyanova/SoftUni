ccde7db87bcc60e457d17e23236ef7d5cb5bd9e1
Stanislava Kaloyanova <st.kaloyanova@gmail.com>
2024-01-25 15:23:29 +0200

    solution added

---
+++ QA_BackEnd-Test-Automation-Jan2024/02.JavaScript-Basics/02-student-information.js
@@ -0,0 +1,8 @@
+function studentInfo(studentName, studentAge, studentGrade) {
+    'use strict'
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${studentGrade.toFixed(2)}`)
+}
+
+studentInfo('John', 15, 5.54678);
+studentInfo('Steve', 16, 2.1426);
+studentInfo('Marry', 12, 6.00);
---
+++ QA_BackEnd-Test-Automation-Jan2024/02.JavaScript-Basics/03-excellent-grade.js
@@ -0,0 +1,11 @@
+function grade(grade) {
+    'use strict'
+    if (grade >= 5.5) {
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+
+grade(5.50)
+grade(4.35)
