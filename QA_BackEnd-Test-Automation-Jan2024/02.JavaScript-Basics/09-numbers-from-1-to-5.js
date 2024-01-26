f7a88939deccc0f549f0889083c189eb632db3aa
Stanislava Kaloyanova <st.kaloyanova@gmail.com>
2024-01-26 14:07:46 +0200

    solution added

---
+++ QA_BackEnd-Test-Automation-Jan2024/02.JavaScript-Basics/09-numbers-from-1-to-5.js
@@ -0,0 +1,9 @@
+function print() {
+    'use strict'
+    
+    for(let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+print();
---
+++ QA_BackEnd-Test-Automation-Jan2024/02.JavaScript-Basics/10-numbers-from-m-to-n.js
@@ -0,0 +1,10 @@
+function print(m, n) {
+    'use strict'
+    
+    for (let i = m; i >= n; i--) {
+        console.log(i);        
+    }
+}
+
+print(6, 2);
+print(4, 1);
