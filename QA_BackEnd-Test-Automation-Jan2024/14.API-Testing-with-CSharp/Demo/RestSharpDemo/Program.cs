4ff6e71b58bbbd24245fc7642ce50ccc77ed4804
Stanislava Kaloyanova <st.kaloyanova@gmail.com>
2024-03-25 14:13:37 +0200

    demo upload

---
+++ QA_BackEnd-Test-Automation-Jan2024/14.API-Testing-with-CSharp/Demo/RestSharpDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+var client = new RestClient("https://api.github.com");
+var request = new RestRequest("/users/skaloyanova/repos", Method.Get);
+var response = client.Execute(request);
+
+Console.WriteLine(response.Content);
+Console.WriteLine(response.StatusCode);
+Console.WriteLine(string.Join("\n", response.Headers));
