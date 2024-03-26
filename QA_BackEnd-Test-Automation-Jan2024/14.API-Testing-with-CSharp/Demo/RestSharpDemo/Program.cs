a389261c32ce62d27e21adda2faacf89728e26f0
Stanislava Kaloyanova <64529172+skaloyanova@users.noreply.github.com>
2024-03-26 10:30:13 +0200

    Delete QA_BackEnd-Test-Automation-Jan2024/14.API-Testing-with-CSharp/Demo directory

--- QA_BackEnd-Test-Automation-Jan2024/14.API-Testing-with-CSharp/Demo/RestSharpDemo/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using RestSharp;
-
-var client = new RestClient("https://api.github.com");
-var request = new RestRequest("/users/skaloyanova/repos", Method.Get);
-var response = client.Execute(request);
-
-Console.WriteLine(response.Content);
-Console.WriteLine(response.StatusCode);
-Console.WriteLine(string.Join("\n", response.Headers));
